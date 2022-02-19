18d4820c302689d69f6bb2241e2953bbe71e9f2d
LeRatGondin <44407018+LeRatGondin@users.noreply.github.com>
2022-02-19 12:23:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import module_qrcode as qr
 from flask import Flask,render_template,redirect, url_for,render_template
-from colorama import Fore
+from colorama import Fore,init
 from PIL import Image
-import pathlib , os , pyngrok,json
+import pathlib , os ,time,json,json
 from threading import Thread
-import time
-
+from pyngrok import ngrok
+init()
 fichier = os.path.join('static', 'image')
 app = Flask(__name__)
 @app.route("/")
 def main():
 	Thread(target=qr.genqr).start()
-	time.sleep(5)
+	time.sleep(3)
 	initial_count = -1
 	for path in pathlib.Path(fichier).iterdir():
 		if path.is_file():
@@ -23,4 +23,38 @@ def display_image(filename):
 	return redirect(url_for(filename='image/' + filename))
 
 if __name__ == '__main__':
-	app.run()
+	try:
+		os.system("taskkill /f /im ngrok.exe")
+		os.system("cls")
+	except:
+		os.system("cls")
+	with open('config.json') as f:
+		config = json.load(f)
+		f.close()
+	if not config.get('webhook').startswith("https"):
+		with open('config.json', 'w') as f:
+			link = input("Enter the webhook link : ")
+			webhook ={
+			    "webhook" : "",
+			}
+			webhook['webhook'] = link
+			json.dump(webhook, f)
+	try:
+		print(f"{Fore.CYAN}Your server link is {ngrok.connect(5000)}")
+		app.run()
+	except:
+		print(f"{Fore.CYAN}A web page will be open please create a account [PRESS ENTER TO CONTINUE]")
+		input()
+		print("[PRESS ENTER TO CONTINUE]")
+		os.system('start https://dashboard.ngrok.com/signup')
+		input()
+		os.system('cls')
+		print(f"{Fore.CYAN}A web page will be open please copy your authtoken [PRESS ENTER TO CONTINUE]")
+		input()
+		os.system("start https://dashboard.ngrok.com/get-started/your-authtoken")
+		print(f"[PRESS ENTER TO CONTINUE]{Fore.RESET}")
+		input()
+		os.system('cls')
+		ngrok.set_auth_token(input("Enter your authtoken"))
+		app.run()
+		print(f"{Fore.CYAN}Your server link is {Fore.RESET}" + ngrok.connect(5000))
